5dc923f2d2a08e0bf125c31ab6fdd3dac6d3cbd9
Moksleivis <rimvydas.banys@stud.kitm.lt>
2024-02-23 08:47:38 +0200

    a

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hello World')
+
+const text = document.querySelector("h1");
+
+const input = document.querySelector('input');
+
+input.addEventListener((e)=>{
+e.preventdefault
+})
+console.log(input.value);
+
